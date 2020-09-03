7e704bc8df2dbcce2281ac7659c71d3646bcbb0f
cykz <cykz@cykz.com>
2020-09-03 18:17:29 +0200

    change to OOP structure

--- mouseMove.py
+++ mouseMove.py
@@ -3,11 +3,15 @@
 
 import time
 import pyautogui
-#print(pyautogui.size())
 
+class Mouse:
+  def move(self):
+    pyautogui.moveTo(100, 100, duration =1)
+    pyautogui.moveTo(200, 200, duration =1)
+    pyautogui.moveTo(300, 300, duration =1)
+    pyautogui.moveTo(400, 400, duration =1)
+    time.sleep(2)
+
+mouse = Mouse()
 while True:
-  pyautogui.moveTo(100, 100, duration =1)
-  pyautogui.moveTo(200, 200, duration =1)
-  pyautogui.moveTo(300, 300, duration =1)
-  pyautogui.moveTo(400, 400, duration =1)
-  time.sleep(2)
+  mouse.move()
